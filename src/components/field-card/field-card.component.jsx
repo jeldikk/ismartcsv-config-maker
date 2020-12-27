7c866127fd211ef163c8010d3a1690b0e5bbc66c
jeldikk <jeldi.kamal2013@gmail.com>
2020-12-28 00:47:55 +0530

    Removed buttons and added context menu to field-card instead, removeField action implemented

--- src/components/field-card/field-card.component.jsx
+++ src/components/field-card/field-card.component.jsx
@@ -4,22 +4,47 @@ import {connect} from "react-redux"
 import {Button} from 'react-bootstrap'
 import "./field-card.styles.scss"
 
+// import {electron} from "../../App"
+const electron = window.require('electron')
+
+const {Menu} = electron.remote;
 // import FIELD_DATA from "./field-card.data"
 
 import {removeField} from "../../redux/fields-info/fields-info.actions"
 
 export class FieldCard extends Component {
+
+    contextMenuHandler = (event) => {
+
+        const contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'edit',
+                click: () => {
+                    console.log("this should open an modal window")
+                }
+            },
+            {
+                label: 'delete',
+                click: () => {
+                    this.props.removeField(this.props.field)
+                }
+            }
+        ])
+
+        contextMenu.popup();
+    }
     render() {
+        // console.log(electron.remote)
         const {field} = this.props;
         return (
-            <div className="field-card">
+            <div className="field-card" onContextMenu={this.contextMenuHandler}>
                 <code>
                     {`name : ${field.name}, colno: ${field.colno}, label: ${field.label}(${field.units}) `}
                 </code>
-                <div className="buttons">
+                {/* <div className="buttons">
                     <Button variant="primary mx-2" size="sm">Edit</Button>
-                    <Button variant="danger mx-2" size="sm" onClick={() => this.props.removeField(field)}>Delete</Button>
-                </div>
+                    <Button variant="danger mx-2" size="sm" onContextMenu={() => console.log("context menu clicked")} onClick={() => this.props.removeField(field)}>Delete</Button>
+                </div> */}
             </div>
         )
     }
